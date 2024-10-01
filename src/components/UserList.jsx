4e731bf3d7e0207cc9145a36abeba318b4d4e06f
GunaKuppuchamy <k.guna0314@gmail.com>
2024-10-01 12:20:31 +0530

    0.01

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -10,7 +10,7 @@ const UserList = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get("http://localhost:3001/ad");
+                const response = await axios.get("http://localhost:3002/ad");
                 setExpense(response.data);
                 // console.log(expense);
             } catch (error) {
@@ -33,7 +33,7 @@ const UserList = () => {
         e.preventDefault();
         if (!isEdit) {
             if (amount != 0) {
-                const response = await axios.post("http://localhost:3001/add", {
+                const response = await axios.post("http://localhost:3002/add", {
                     category: category,
                     amount: amount
                 });
@@ -48,7 +48,7 @@ const UserList = () => {
                 category: category,
                 amount: amount
             }
-            alert(id)
+            // alert(id)
             axios.put(`http://localhost:3002/${id}`, data).then((res) => {
                 // alert(res);
                 setExpense([...res.data]);
@@ -63,11 +63,10 @@ const UserList = () => {
     }
     const handleEdit = (exp) => {
         setId(exp._id)
-        // alert(id)
         setCategory(exp.category);
         setAmount(exp.amount);
         setIsEdit(true);
-        // handleChange();
+        handleChange();
 
 
 
@@ -86,12 +85,10 @@ const UserList = () => {
     const handleDelete = (exp) => {
         // setId(exp._id);
         
-        // console.log(exp);
+        console.log(exp);
         axios.delete(`http://localhost:3002/${exp}`)
             .then((res) => {
-                // alert(res.data)
-                toast.success(res);
-
+                alert(res.data)
             }).catch((err) => { console.log(err) });
             // alert("delete called");
         // let deletedArray = expense.filter((data) => data.id != id);
