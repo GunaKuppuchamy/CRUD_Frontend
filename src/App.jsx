3d84f34959ca63f86e8d431a9b674a8b801a4de4
GunaKuppuchamy <k.guna0314@gmail.com>
2024-11-04 07:24:33 +0530

    0.0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from './components/About.jsx';
 import Signup from './components/Signup.jsx';
 import Login from './components/Login.jsx';
 import UserList from './components/UserList'
-import Demo from './demo.jsx';
 // import { v4 as uuidv4 } from 'uuid';
 
 
@@ -15,8 +14,6 @@ function App() {
       <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* <Route path="/" element={<Demo />} /> */}
-
         <Route path="/about" element={<About />} />
         <Route path="/home" element={<UserList />} />
         <Route path="/signup" element={<Signup />} />
